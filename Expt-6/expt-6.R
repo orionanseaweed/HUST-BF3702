5aac7f7e944c150bf99aeb77b10e698750c582b0
Kane <concealedcharacterx@gmail.com>
2025-04-22 00:48:42 +0700

    Update expt-6.R

--- Expt-6/expt-6.R
+++ Expt-6/expt-6.R
@@ -1,18 +1,18 @@
 library(ggplot2)
 library(tidyverse)
 
-# Tạo dataframe
+# tạo data frame từ số liệu thực nghiệm
 data <- data.frame(
   Mẫu = rep(c("R", "S"), each = 5),
   Số_tế_bào = c(37, 30, 30, 23, 37, 66, 58, 78, 79, 74)
 )
 
-# Tính trung bình và độ lệch chuẩn
+# tính trung bình và độ lệch chuẩn
 data_summary <- data |> 
   dplyr::group_by(Mẫu) |> 
   dplyr::summarize(Trung_binh = mean(Số_tế_bào), SD = sd(Số_tế_bào), .groups = "drop")
 
-# Vẽ biểu đồ với thanh sai số
+# vẽ biểu đồ với thanh sai số
 ggplot(data_summary, aes(x = Mẫu, y = Trung_binh)) +
   geom_bar(stat = "identity", position = position_dodge(0.9), fill = c("#1f77b4", "#ff7f0e"), color = "black") +
   geom_errorbar(aes(ymin = Trung_binh - SD, ymax = Trung_binh + SD),
